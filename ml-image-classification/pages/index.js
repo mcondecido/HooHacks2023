a745ffcc80f98af656d282b6f620dba7f971a1b0
Robert Sun <robertssun1234@gmail.com>
2023-03-25 18:43:10 -0400

    made heading look nicer

--- ml-image-classification/pages/index.js
+++ ml-image-classification/pages/index.js
@@ -12,49 +12,53 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <h1 style={{fontFamily: 'Myriad Pro Condensed Italic' }}>HooHacks2023</h1>
-      <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper">
-            <textarea></textarea>
-            <hr />
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
+    <div className="App" style={{
+      //backgroundColor: 'lightgreen'
+    }}>
+      <nav>
+        <h1 class="homeHeader" style={{fontFamily: 'Myriad Pro Condensed Italic' }}>RecycleAI</h1>
+      </nav>
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+        >
+          {({
+            imageList,
+            onImageUpload,
+            onImageUpdate,
+            onImageRemove,
+            isDragging,
+            dragProps,
+          }) => (
+            // write your building UI
+            <div className="upload__image-wrapper">
               
-            >
-              Click or Drop here
-            </button>
-            
-            &nbsp;
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="100" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
-                  <button onClick={() => onImageRemove(index)}>Remove</button>
+              <hr />
+              <button
+                style={isDragging ? { color: 'red' } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+                
+              >
+                Click or Drop here
+              </button>
+              
+              &nbsp;
+              {imageList.map((image, index) => (
+                <div key={index} className="image-item">
+                  <img src={image['data_url']} alt="" width="100" />
+                  <div className="image-item__btn-wrapper">
+                    <button onClick={() => onImageUpdate(index)}>Update</button>
+                    <button onClick={() => onImageRemove(index)}>Remove</button>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ImageUploading>
+              ))}
+            </div>
+          )}
+        </ImageUploading>
     </div>
   );
 }
